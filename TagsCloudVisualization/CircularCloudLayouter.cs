efa7d3c792be5316c5540f5f1795efbfea973e2c
Starcev <starcev_misha@mail.ru>
2017-10-28 15:42:46 +0500

    Работает

--- TagsCloudVisualization/CircularCloudLayouter.cs
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Windows.Forms;
 using FluentAssertions;
 using NUnit.Framework;
 
@@ -10,9 +9,9 @@ namespace TagsCloudVisualization
 {
     public class CircularCloudLayouter
     {
-        public Point cloudCenter;
-        public List<Rectangle> rectangleList;
-        private Random random = new Random();
+        public Point CloudCenter;
+        public List<Rectangle> RectangleList;
+        private readonly Random random = new Random();
         private double radius = 10;
         private int dDeg = 10;
         private int dRadius = 5;
@@ -22,8 +21,8 @@ namespace TagsCloudVisualization
             if (center.X < 0 || center.Y < 0)
                 throw new ArgumentException("Coordinates must be nonnegative.");
 
-            cloudCenter = center;
-            rectangleList = new List<Rectangle>();
+            CloudCenter = center;
+            RectangleList = new List<Rectangle>();
         }
 
         public Rectangle PutNextRectangle(Size rectangleSize)
@@ -32,18 +31,17 @@ namespace TagsCloudVisualization
                 throw new ArgumentException("Size should be non-negative");
 
             Rectangle newRectagle;
-            if (rectangleList.Count == 0)
+            if (RectangleList.Count == 0)
             {
                 radius = Math.Min(rectangleSize.Height, rectangleSize.Width) / 2;
-                newRectagle = new Rectangle(new Point(cloudCenter.X - rectangleSize.Width / 2,
-                        cloudCenter.Y - rectangleSize.Height / 2),
+                newRectagle = new Rectangle(new Point(CloudCenter.X - rectangleSize.Width / 2,
+                        CloudCenter.Y - rectangleSize.Height / 2),
                     rectangleSize);
             }
             else
-            {
                 newRectagle = FindPositionOfNewRectangle(rectangleSize);
-            }
-            rectangleList.Add(newRectagle);
+
+            RectangleList.Add(newRectagle);
             return newRectagle;
         }
 
@@ -57,14 +55,14 @@ namespace TagsCloudVisualization
                 var startDeg = random.Next(360);
                 for (var deg = startDeg; deg < startDeg + 360; deg += dDeg)
                 {
-                    var rad = (double) deg / Math.PI * 180.0;
-                    var newRectangleCenterX = (int) (cloudCenter.X + curentRadius * Math.Cos(rad));
-                    var newRectangleCenterY = (int) (cloudCenter.Y + curentRadius * Math.Sin(rad));
+                    var rad = deg / Math.PI * 180.0;
+                    var newRectangleCenterX = (int) (CloudCenter.X + curentRadius * Math.Cos(rad));
+                    var newRectangleCenterY = (int) (CloudCenter.Y + curentRadius * Math.Sin(rad));
 
                     var leftTopX = newRectangleCenterX - rectangleSize.Width / 2;
                     var leftTopY = newRectangleCenterY - rectangleSize.Height / 2;
 
-                    if (leftTopX < 0 || leftTopY/2 < 0)
+                    if (leftTopX < 0 || leftTopY / 2 < 0)
                         continue;
                     var candidate = new Rectangle(new Point(leftTopX, leftTopY), rectangleSize);
                     if (!ColisionWithOtherRectangles(candidate))
@@ -85,8 +83,8 @@ namespace TagsCloudVisualization
             var g = Graphics.FromImage(bitmap);
 
             var selPen = new Pen(Color.Blue);
-            g.DrawRectangle(new Pen(Color.Red), cloudCenter.X, cloudCenter.Y, 1, 1);
-            foreach (var rectangle in rectangleList)
+            g.DrawRectangle(new Pen(Color.Red), CloudCenter.X, CloudCenter.Y, 1, 1);
+            foreach (var rectangle in RectangleList)
                 g.DrawRectangle(selPen, rectangle);
             g.Dispose();
             bitmap.Save("1.bmp");
@@ -94,7 +92,7 @@ namespace TagsCloudVisualization
 
         public bool ColisionWithOtherRectangles(Rectangle candidate)
         {
-            return rectangleList.Any(rectangle => rectangle.IntersectsWith(candidate));
+            return RectangleList.Any(rectangle => rectangle.IntersectsWith(candidate));
         }
     }
 
@@ -112,7 +110,7 @@ namespace TagsCloudVisualization
         public void PutNextRectangle_ThrowArgumentException_OnNonPositiveSize()
         {
             var layout = new CircularCloudLayouter(new Point(10, 10));
-            var size = new Size(-10,10);
+            var size = new Size(-10, 10);
             Assert.Throws<ArgumentException>(() => layout.PutNextRectangle(size));
         }
 
@@ -121,7 +119,7 @@ namespace TagsCloudVisualization
         {
             var layout = new CircularCloudLayouter(new Point(10, 10));
             layout.PutNextRectangle(new Size(20, 25));
-            layout.rectangleList.Count.Should().Be(1);
+            layout.RectangleList.Count.Should().Be(1);
         }
 
         [Test]
@@ -130,10 +128,10 @@ namespace TagsCloudVisualization
             var layout = new CircularCloudLayouter(new Point(100, 100));
             layout.PutNextRectangle(new Size(10, 10));
 
-            var firstRectangle = layout.rectangleList[0];
+            var firstRectangle = layout.RectangleList[0];
             var centerOfRectangle = new Point(firstRectangle.X + firstRectangle.Width / 2,
                 firstRectangle.Y + firstRectangle.Height / 2);
-            centerOfRectangle.ShouldBeEquivalentTo(layout.cloudCenter);
+            centerOfRectangle.ShouldBeEquivalentTo(layout.CloudCenter);
         }
 
 
@@ -144,16 +142,16 @@ namespace TagsCloudVisualization
             for (var i = 0; i < 10; i++)
                 layout.PutNextRectangle(new Size(10, 10));
             layout.DrawCloudTag();
-            pairwiseIntersection(layout).Should().BeFalse();
+            PairwiseIntersection(layout).Should().BeFalse();
         }
 
-        private static bool pairwiseIntersection(CircularCloudLayouter layout)
+        private static bool PairwiseIntersection(CircularCloudLayouter layout)
         {
             var colision = false;
-            for (var i = 0; i < layout.rectangleList.Count; i++)
+            for (var i = 0; i < layout.RectangleList.Count; i++)
             {
-                for (var j = 0; j < layout.rectangleList.Count; j++)
-                    if (layout.rectangleList[j].IntersectsWith(layout.rectangleList[i]) && i != j)
+                for (var j = 0; j < layout.RectangleList.Count; j++)
+                    if (layout.RectangleList[j].IntersectsWith(layout.RectangleList[i]) && i != j)
                     {
                         colision = true;
                         break;
