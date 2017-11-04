398589424dff15b6f72c22b20189e3d187dd5b87
Starcev <starcev_misha@mail.ru>
2017-11-04 22:11:13 +0500

    Добавил тесты

--- TagsCloudVisualization/RectangleExteinsions.cs
+++ TagsCloudVisualization/RectangleExteinsions.cs
@@ -32,43 +32,5 @@ namespace TagsCloudVisualization
         
     }
 
-    [TestFixture]
-    public class RectangleExteinsions_Should
-    {
-        [Test]
-        public void IntersectWithAny_Test()
-        {
-            var rectangles = new List<Rectangle>
-            {
-                new Rectangle(5, 5, 10, 10),
-                new Rectangle(15, 15, 10, 10)
-            };
-            var myRectangle = new Rectangle(12,12, 10,10);
-            myRectangle.IntersectWithAny(rectangles).Should().BeTrue();
-        }
-
-        [Test]
-        public void DistanceTo_Test()
-        {
-            var myRectangle = new Rectangle(0,0,10,10);
-            var center = new Vector(9,8);
-            myRectangle.DistanceTo(center).Should().Be(5);
-        }
-
-        [Test]
-        public void TopLeft_Test()
-        {
-            var rectangle = new Rectangle(10,10,10,10);
-            var expectedTopLeft = new Vector(10,10);
-            rectangle.TopLeft().Should().Be(expectedTopLeft);
-        }
-
-        [Test]
-        public void Center_Test()
-        {
-            var rectangle = new Rectangle(0, 0, 10, 10);
-            var expectedCenter = new Vector(5, 5);
-            rectangle.Center().Should().Be(expectedCenter);
-        }
-    }
+   
 }
