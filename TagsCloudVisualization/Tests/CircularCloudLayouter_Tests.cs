ab11ac2340d33e041921e866d4694dfefb0bfa49
Starcev <starcev_misha@mail.ru>
2017-11-04 22:11:01 +0500

    Вынес тесты в отдельную папку.

---
+++ TagsCloudVisualization/Tests/CircularCloudLayouter_Tests.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    public class CircularCloudLayouter_Tests
+    {
+        [TestCase(-1, 3, TestName = "Negative X coordinate")]
+        [TestCase(1, -3, TestName = "Negative Y coordinate")]
+        public void TestConstructor_ThrowArgumentException(int x, int y)
+        {
+            var cloudCenter = new Point(x, y);
+            Assert.Throws<ArgumentException>(() => new CircularCloudLayouter(cloudCenter));
+        }
+
+        [TestCase(-10, 10, TestName = "Negative width")]
+        [TestCase(10, -10, TestName = "Negative height")]
+        [TestCase(0, 10, TestName = "Zero width")]
+        [TestCase(10, 0, TestName = "Zero height")]
+        public void PutNextRectangle_ThrowArgumentException_OnNonPositiveSize(int width, int height)
+        {
+            var layout = new CircularCloudLayouter(new Point(10, 10));
+            var size = new Size(width, height);
+            Assert.Throws<ArgumentException>(() => layout.PutNextRectangle(size));
+        }
+
+        [Test]
+        public void PutNextRectangle_ShouldReturnRectangle_WithCorrectSize()
+        {
+            var layout = new CircularCloudLayouter(new Point(10, 10));
+            var rectangle = layout.PutNextRectangle(new Size(20, 25));
+            rectangle.Size.ShouldBeEquivalentTo(new Size(20, 25));
+        }
+
+        private static readonly object[] RectanglesCases =
+        {
+            new object[] { new []{(1,2), (3,4)}, 2},
+            new object[] { new []{ (1, 2), (3, 4), (4, 2) }, 3},
+            new object[] { new []{ (1, 2), (3, 4), (4, 2), (5, 7), (10, 15), (1, 2) }, 6},
+        };
+
+        [Test, TestCaseSource("RectanglesCases")]
+        public void PutNextRectangle_AddManyRectangles((int, int)[] sizePairsArray, int expectedResult)
+        {
+            var layout = new CircularCloudLayouter(new Point(10, 10));
+            var rectangleList = new List<Rectangle>();
+            foreach (var sizePair in sizePairsArray){
+                rectangleList.Add(layout.PutNextRectangle(new Size(sizePair.Item1, sizePair.Item2)));
+            }
+            rectangleList.Count.Should().Be(expectedResult);
+        }
+
+        [Test]
+        public void PutNextRectangle_FirstRectangle_ShouldBeOnCloudCenter()
+        {
+            var cloudCenter = new Point(100, 100);
+            var layout = new CircularCloudLayouter(cloudCenter);
+            var firstRectangle = layout.PutNextRectangle(new Size(10, 10));
+            var centerOfRectangle = new Point(firstRectangle.X + firstRectangle.Width / 2,
+                firstRectangle.Y + firstRectangle.Height / 2);
+
+            centerOfRectangle.ShouldBeEquivalentTo(cloudCenter);
+        }
+
+        [Test]
+        public void PutNextRectangle_TwoAddedRectangles_ShouldNotIntersect()
+        {
+            var layout = new CircularCloudLayouter(new Point(10, 10));
+            var firstRectangle = layout.PutNextRectangle(new Size(20, 25));
+            var secondRectangle = layout.PutNextRectangle(new Size(20, 25));
+            firstRectangle.IntersectsWith(secondRectangle).Should().BeFalse();
+        }
+
+        [Test]
+        public void PutNextRectangle_ShouldReturnRectanglesWithCorrectSize()
+        {
+            var size = new Size(20, 25);
+            var layout = new CircularCloudLayouter(new Point(10, 10));
+            var rectangleList = new List<Rectangle>();
+            for (var i = 0; i < 100; i++)
+                rectangleList.Add(layout.PutNextRectangle(size));
+
+            var isCorrectSize = rectangleList.All(rectangle => Equals(rectangle.Size, size));
+            isCorrectSize.Should().BeTrue();
+        }
+
+
+        [Test]
+        public void PutNextRectangle_PairwiseIntersectionShouldBeFalse_OnBigNumberOfRectangles()
+        {
+            var layout = new CircularCloudLayouter(new Point(10, 10));
+            var rectangleList = new List<Rectangle>();
+            for (var i = 0; i < 100; i++)
+                rectangleList.Add(layout.PutNextRectangle(new Size(10, 10)));
+
+            PairwiseIntersection(rectangleList).Should().BeFalse();
+        }
+
+        [Test, Timeout(1000)]
+        public void Timeout_AddALotOfRectangles()
+        {
+            var layout = new CircularCloudLayouter(new Point(10, 10));
+            for (var i = 1; i < 400; i++)
+                layout.PutNextRectangle(new Size(10, 10));
+        }
+
+        [Test]
+        public void PutNextRectangle_AllRectanglesShouldBeInTheFormOfNotBigCircle()
+        {
+            const int rectangleCount = 100;
+            const int rectangleSize = 10;
+            // Общая площадь всех прямоугольников = 100*10*10 
+            // Радиус круга с такой площадью есть sqrt(100*10*10/pi)
+            // Так как Облако из прямоугольников, возьмем 1.5 таких радиуса
+            var cloudCenter = new Point(50, 50);
+            var layout = new CircularCloudLayouter(cloudCenter);
+            var maxDistance = 0.0;
+            for (var i = 0; i < rectangleCount; i++)
+            {
+                var rectangle = layout.PutNextRectangle(new Size(rectangleSize, rectangleSize));
+                var distance = rectangle.DistanceTo(cloudCenter);
+                if (distance > maxDistance)
+                    maxDistance = distance;
+            }
+            var expectedRadius = 1.5*Math.Sqrt(rectangleCount *rectangleSize * rectangleSize / Math.PI);
+            maxDistance.Should().BeLessThan(expectedRadius);
+        }
+
+
+
+        private static bool PairwiseIntersection(List<Rectangle> rectangleList)
+        {
+            for (var i = 0; i < rectangleList.Count; i++)
+                for (var j = i + 1; j < rectangleList.Count; j++)
+                    if (rectangleList[j].IntersectsWith(rectangleList[i]))
+                        return true;
+            return false;
+        }
+        
+    }
+    
+}
---
+++ TagsCloudVisualization/Tests/Vector_Tests.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Drawing;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace TagsCloudVisualization
+{
+    [TestFixture]
+    public class Vector_Tests
+    {
+        [Test]
+        public void Vector_AdditionTest()
+        {
+            var a = new Vector(2, 3);
+            var b = new Vector(1, 7);
+            var sum = a + b;
+            var expectedSum = new Vector(3, 10);
+            sum.Should().Be(expectedSum);
+        }
+
+        [Test]
+        public void Vector_SubtractionTest()
+        {
+            var a = new Vector(2, 3);
+            var b = new Vector(1, 7);
+            var diff = a - b;
+            var expectedDiff = new Vector(1, -4);
+            diff.Should().Be(expectedDiff);
+        }
+
+        [Test]
+        public void Vector_ShouldThrowArgumentException_WhenDividedByZero()
+        {
+            var a = new Vector(2, 3);
+            Vector b = a / 0;
+        }
+
+        [Test]
+        public void Vector_MultiplicationByAConstantTest()
+        {
+            var a = new Vector(2, 3);
+            var product = 2 * a;
+            var expected = new Vector(4, 6);
+            product.Should().Be(expected);
+        }
+
+        [Test]
+        public void Vector_DivideByAConstantTest()
+        {
+            var a = new Vector(2, 4);
+            var division = a / 2;
+            var expected = new Vector(1, 2);
+            division.Should().Be(expected);
+        }
+
+        [Test]
+        public void Vector_LengthTest()
+        {
+            var vector = new Vector(3, 4);
+            var expectedLength = 5;
+            vector.Length().Should().Be(expectedLength);
+        }
+        [Test]
+        public void Vector_ReturnCorrectAngle()
+        {
+            var angle = Vector.Angle(Math.PI);
+            var expectedAngle = new Vector(-1, 0);
+            angle.Should().Be(expectedAngle);
+        }
+
+        [Test]
+        public void Vector_CastToPointTest()
+        {
+            var point = new Point(1, 2);
+            var vector = new Vector(1, 2);
+            var pointVector = (Point)(vector);
+            pointVector.Should().Be(point);
+        }
+
+        [Test]
+        public void Vector_CastToSizeTest()
+        {
+            var size = new Size(1, 2);
+            var vector = new Vector(1, 2);
+            var pointVector = (Size)(vector);
+            pointVector.Should().Be(size);
+        }
+    }
+}
